5d02afad985d6484e7da723ebe8e42ebeeb1cf1b
Sandeep <sandeepwadhawan2001@gmail.com>
2024-10-27 00:10:20 +0530

    Added render URL in SingleChat.js endpoint

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import Lottie from 'react-lottie';
 import animationData from '../animations/typing.json';
 
 import io from 'socket.io-client';
-const ENDPOINT = 'http://localhost:3000';
+const ENDPOINT = 'https://chat-burst.onrender.com';
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
