5934772bb060341710f718bc4cf6dfda9bce295f
Sandeep <sandeepwadhawan2001@gmail.com>
2024-10-26 21:14:11 +0530

    Some changes for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+const dotenv = require('dotenv');
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require('colors');
@@ -9,19 +10,16 @@ const messageRoutes = require('./routes/messageRoutes');
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require('path');
 
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
 
 connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is Running Successfully');
-});
 
 
 // app.get('/api/chat', (req, res) => {
@@ -38,7 +36,22 @@ app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
+// ------------------------Deployment----------------------
 
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is Running Successfully');
+    });
+}
+
+// ------------------------Deployment----------------------
 app.use(notFound);
 app.use(errorHandler);
 
